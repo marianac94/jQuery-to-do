bd7ee0960b756569ca477f2f8a9f668978450c33
Mariana <marianac94@hotmail.com>
2018-09-24 13:49:32 -0500

    creates a todo, move a todo, still have to make a remove

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// need to find a button and make a click function on it.
+$('button').on('click', () => {
+  const $info = $('input').val();
+    console.log($info);
+    createTodo($info);
+})
+
+const createTodo = (inputValue) => {
+  const $div = $('<div/>').addClass('todos');
+
+    $div.append('<h5>' + inputValue + '</h5>')
+
+    const $button = $('<button/>').on('click', completedTodo);
+      $button.text('completed');
+
+    $div.append($button)
+      $('#col-1').append($div)
+
+}
+
+const completedTodo = (event) => {
+  const $currentTodo = $(event.currentTarget).parent()
+    console.log($currentTodo);
+
+    $('#col-2').append($currentTodo)
+}
+
+const 
